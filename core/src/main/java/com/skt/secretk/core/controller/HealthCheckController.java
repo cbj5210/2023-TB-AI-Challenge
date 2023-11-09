b8a03f3d54d2a1684216e54eb9468fd9fb6ec462
최병준 <cbj5210@nate.com>
2023-11-09 11:13:18 +0900

    add gcp nlp service

--- core/src/main/java/com/skt/secretk/core/controller/HealthCheckController.java
+++ core/src/main/java/com/skt/secretk/core/controller/HealthCheckController.java
@@ -2,13 +2,20 @@ package com.skt.secretk.core.controller;
 
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
+import com.skt.secretk.core.service.GcpNlpService;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthCheckController {
+    // todo : remove
+    private final GcpNlpService gcpNlpService;
 
     @GetMapping("/health")
     public String healthCheck() {
@@ -31,5 +38,11 @@ public class HealthCheckController {
         db.collection("TBAI").add(item);
 
         return "OK";
+    }
+
+    //TODO : remove
+    @GetMapping("/test2/{message}")
+    public void test2(@PathVariable String message) {
+        gcpNlpService.query(message);
     }
 }
--- core/src/main/java/com/skt/secretk/core/service/GcpNlpService.java
+++ core/src/main/java/com/skt/secretk/core/service/GcpNlpService.java
@@ -1,8 +1,15 @@
 package com.skt.secretk.core.service;
 
+import com.skt.secretk.core.model.GoogleNlp;
+import com.skt.secretk.core.model.GoogleNlp.Entity;
+import com.skt.secretk.core.util.StreamUtils;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +18,25 @@ public class GcpNlpService {
     //TODO : 최병준
     public List<String> query(String message) {
 
-        return null;
+        try {
+            File file = ResourceUtils.getFile("classpath:googleNplApiKey.txt");
+            Scanner scanner = new Scanner(file);
+            String apiKey = scanner.nextLine();
+
+            // todo : api call
+
+            // for test
+            GoogleNlp googleNlp = new GoogleNlp();
+
+            return StreamUtils.ofNullable(googleNlp.getEntities())
+                             .map(Entity::getName)
+                             .toList();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return new ArrayList<>();
+        }
     }
 }
