e4269d61b70be35dd47c8cbe1297881dccbcef00
정주상님/Campaign Platform팀 <jusang.jung@sktelecom.com>
2023-11-13 15:24:02 +0900

    Tnet 화면 시인성 개선

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatBot from "./ChatBot";
+import Tnet from "./Tnet";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<ChatBot />} />
-          <Route path="/:idParam" element={<ChatBot />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<ChatBot />} />
+        <Route path="/:idParam" element={<ChatBot />} />
+        <Route path="/tnet/:idParam" element={<Tnet />} />
+      </Routes>
+    </Router>
   );
 }
 
